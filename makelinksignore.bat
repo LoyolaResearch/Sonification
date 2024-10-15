35eafadd64afb84c95d932e6f9da3849daef44a8
Scott McDermott <smcdermo@loyno.edu>
2024-10-15 17:47:28 -0500

    fix bat

--- makelinksignore.bat
+++ makelinksignore.bat
@@ -1,8 +1,15 @@
 @echo off
+
+echo If prompted to delete something (..\Libraries\*), it is safe to answer Y or N.
+echo This just means the symbolic links already exists correctly.
+echo.
+
 cd ChessSonification
+del Libraries
 mklink /d "Libraries" "..\Libraries"
 cd ..
 cd NumbersSonification
+del Libraries
 mklink /d "Libraries" "..\Libraries"
 
 echo.
